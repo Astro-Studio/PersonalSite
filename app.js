fc594d8ebb3d68ded76cd8d7ba6ad7520dca90a0
Ayham <61061943+Astro-Studio@users.noreply.github.com>
2020-10-11 21:18:10 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,116 @@
+var swiperAnimation = new SwiperAnimation();
+
+var swiperH = new Swiper(".swiper-container-h", {
+  pagination: {
+    el: ".swiper-pagination-h",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  speed: 1100,
+
+  on: {
+    init: function () {
+      swiperAnimation.init(this).animate();
+    },
+    slideChange: function () {
+      swiperAnimation.init(this).animate();
+    },
+  },
+});
+
+var swiperV = new Swiper(".swiper-container-v", {
+  direction: "vertical",
+  mousewheel: true,
+  loop: true,
+  spaceBetween: 20,
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+});
+
+// ——————————————————————————————————————————————————
+// TextScramble
+// ——————————————————————————————————————————————————
+
+class TextScramble {
+  constructor(el) {
+    this.el = el;
+    this.chars = "!<>-_\\/[]{}—=+*^?#________";
+    this.update = this.update.bind(this);
+  }
+  setText(newText) {
+    const oldText = this.el.innerText;
+    const length = Math.max(oldText.length, newText.length);
+    const promise = new Promise((resolve) => (this.resolve = resolve));
+    this.queue = [];
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || "";
+      const to = newText[i] || "";
+      const start = Math.floor(Math.random() * 40);
+      const end = start + Math.floor(Math.random() * 40);
+      this.queue.push({ from, to, start, end });
+    }
+    cancelAnimationFrame(this.frameRequest);
+    this.frame = 0;
+    this.update();
+    return promise;
+  }
+  update() {
+    let output = "";
+    let complete = 0;
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      let { from, to, start, end, char } = this.queue[i];
+      if (this.frame >= end) {
+        complete++;
+        output += to;
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.28) {
+          char = this.randomChar();
+          this.queue[i].char = char;
+        }
+        output += `<span class="dud">${char}</span>`;
+      } else {
+        output += from;
+      }
+    }
+    this.el.innerHTML = output;
+    if (complete === this.queue.length) {
+      this.resolve();
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update);
+      this.frame++;
+    }
+  }
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)];
+  }
+}
+
+//Texts
+const phrases = ["Hello World!", "<< Swipe Left"];
+
+const el = document.querySelector(".text");
+const fx = new TextScramble(el);
+
+let counter = 0;
+const next = () => {
+  fx.setText(phrases[counter]).then(() => {
+    setTimeout(next, 3800);
+  });
+  counter = (counter + 1) % phrases.length;
+};
+
+next();
+
+// ——————————————————————————————————————————————————
+// Ribons
+// ——————————————————————————————————————————————————
